ede9e637132eecb199a46de188575a2dd197d5df
Cooper Myton <cooper.myton@gmail.com>
2026-04-04 22:00:19 -0700

    End of tutorial

--- RollerBall/Assets/scripts/PlayerController.cs
+++ RollerBall/Assets/scripts/PlayerController.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
-
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
 
@@ -14,6 +17,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -24,11 +30,30 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 10)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
         rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
     }
 
 }
